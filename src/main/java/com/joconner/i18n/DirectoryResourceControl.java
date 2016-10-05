52f95307c0e4ed5052cef94b8f901a920603850a
John O'Conner <john_oconner@intuit.com>
2016-10-04 17:16:18 -0700

    Provide javadoc.

--- src/main/java/com/joconner/i18n/DirectoryResourceControl.java
+++ src/main/java/com/joconner/i18n/DirectoryResourceControl.java
@@ -5,6 +5,18 @@ import java.util.ResourceBundle;
 
 
 /**
+ * DirectoryResourceControl is a ResourceBundle controller that helps a ResourceBundle
+ * factory find resources in subdirectories of the base resource file. For example, if
+ * your baseName resource is com.example.res.Foo, then this controller will help a
+ * ResourceBundle find translations in subpackages that are named for the target locale. For example,
+ * resources for the fr-CA language will be in the following:
+ *     com.example.res.fr-ca.Foo
+ *
+ * Resources for Mexican Spanish (es-MX) should be stored here:
+ *     com.example.res.es-mx.FOO
+ *
+ * Resource subpackage names should be lowercase BCP-47 language tag identifiers.
+ *
  * @author joconner
  */
 public class DirectoryResourceControl extends ResourceBundle.Control {
@@ -14,7 +26,11 @@ public class DirectoryResourceControl extends ResourceBundle.Control {
 
     }
 
-
+    /**
+     * Convert a baseName resource file of the form package-name.ResourceName to
+     * package-name.locale-string.ResourceName. This controller separates localized resources into
+     * their own subpackage and does extend the ResourceName.
+     */
     @Override
     public String toBundleName(String baseName, Locale locale) {
         int nBasePackage = baseName.lastIndexOf(".");
